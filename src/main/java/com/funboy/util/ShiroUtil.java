bcfffbb8ddbeca3090033b46a3c5be221ee9164e
Lagran <niefanbo@vip.qq.com>
2022-04-29 00:13:18 +0800

    Shiro util.

---
+++ src/main/java/com/funboy/util/ShiroUtil.java
@@ -0,0 +1,12 @@
+package com.funboy.util;
+
+import com.funboy.shiro.AccountProfile;
+import org.apache.shiro.SecurityUtils;
+
+public class ShiroUtil {
+
+    public static AccountProfile getProfile() {
+        return (AccountProfile) SecurityUtils.getSubject().getPrincipal();
+    }
+
+}
